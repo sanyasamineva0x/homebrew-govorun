7c8a48a9b719df1c8ee256281cfcb08916895c34
sanyasamineva0x <sanyasamineva@gmail.com>
2026-03-18 17:58:14 +0300

    bump: v0.1.3 rebuild (clean DerivedData)

--- Casks/govorun.rb
+++ Casks/govorun.rb
@@ -1,6 +1,6 @@
 cask "govorun" do
   version "0.1.3"
-  sha256 "6efae8b11a4a5ba150f981cb4830ad5e3c825f2192d755b99cd9a3c1a0d907ca"
+  sha256 "3cfcf46da0d2ba58e45fd9e7ec789447d8efa1f7490aa43be2f08a4f6c32ff7c"
 
   url "https://github.com/sanyasamineva0x/govorun-app/releases/download/v#{version}/Govorun.dmg"
   name "Говорун"
