0f6b586ed3a646a1e33eafa1563e322ee68f9bda
sanyasamineva0x <sanyasamineva@gmail.com>
2026-03-24 00:09:19 +0300

    bump: govorun 0.1.10

--- Casks/govorun.rb
+++ Casks/govorun.rb
@@ -1,6 +1,6 @@
 cask "govorun" do
-  version "0.1.9"
-  sha256 "c4cd532f6f1f5246f13078245a7f049005f5232af381af9bebf513bb545deb51"
+  version "0.1.10"
+  sha256 "23ffca226c68acc03e3beb52d8a022bffafaaae57519be5d0716bdb57934ef98"
 
   url "https://github.com/sanyasamineva0x/govorun-app/releases/download/v#{version}/Govorun.dmg"
   name "Говорун"
